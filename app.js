efdd20abc3af51ac00d33831f681a6ff1bb155c4
King-JuJu <joanrey2002@gmail.com>
2022-02-01 19:38:20 -0300

    cambio footer color iconos y app.js heroku

--- app.js
+++ app.js
@@ -17,5 +17,8 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'))
 });
 
-app.listen(3030, () => console.log("Servidor en funcionamiento"));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo en el puerto 3000")
+})
+
 
